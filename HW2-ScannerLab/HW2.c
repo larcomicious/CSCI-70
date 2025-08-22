e48272db37179762a8bf48d479e3b3b8164b1209
larcomicious <marco.gimena@student.ateneo.edu>
2025-08-22 22:59:31 +0800

    Implemented pattern finding in filenames in directory

--- HW2-ScannerLab/HW2.c
+++ HW2-ScannerLab/HW2.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <dirent.h>
 
+#define BUFFER_SIZE 1024
 
-void scanner(const char filename[]) {
+// got from stack overflow. replaces substring with specified pattern and dynamically allocates to new 
+// char array if substring and pattern are not same length
+// https://stackoverflow.com/questions/56043612/how-to-replace-a-part-of-a-string-with-another-substring
+char* strreplace(char *s, const char *s1, const char *s2) {
+    char *p = strstr(s, s1);
+    if (p != NULL) {
+        size_t len1 = strlen(s1);
+        size_t len2 = strlen(s2);
+        if (len1 != len2)
+            memmove(p + len2, p + len1, strlen(p + len1) + 1);
+        memcpy(p, s2, len2);
+    }
+    return s;
+}
+
+void scanner(char* filename, const char* pattern) {
     // input
     FILE *in;
-    char input[100]; // input buffer, arbitrary size
+    char input[BUFFER_SIZE]; // input buffer
     in = fopen(filename, "r");
-    fgets(input, 100, in);
+    fgets(input, BUFFER_SIZE, in);
     fclose(in);
     
     // output
+    char* outputFilename = strreplace(filename, pattern, "output");
     FILE *out;
-    out = fopen("output.txt", "a");
+    out = fopen(outputFilename, "a");
 
+    // scanning
     int i = 0;
     while (input[i] != '\0') {
         char ch = input[i];
@@ -25,7 +45,7 @@ void scanner(const char filename[]) {
         }
         // if digit
         else if (isdigit(ch)) {
-            char numBuffer[100]; // arbitrary size
+            char numBuffer[BUFFER_SIZE];
             int index = 0;
             
             // look for succeeding digits
@@ -68,7 +88,27 @@ void scanner(const char filename[]) {
     }
 }
 
+void application(const char* pattern) {
+    DIR *d;
+    struct dirent *dir;
+    const char *search_dir = "."; // current directory
+
+    d = opendir(search_dir);
+    if (d) {
+        // go through all files in search_dir directory
+        while ((dir = readdir(d)) != NULL) {
+            char* filename = dir->d_name;
+            // check if filename has pattern
+            if (strstr(filename, pattern)) {
+                // run scanner
+                scanner(filename, pattern);
+            }
+        }
+        closedir(d);
+    }
+}
+
 int main() {
-    scanner("input.txt");
+    application("input");
     return 0;
 }
