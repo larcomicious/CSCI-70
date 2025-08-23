d8991df4bb8749a20ace192088aabc78a490e144
larcomicious <marco.gimena@student.ateneo.edu>
2025-08-23 08:15:51 +0800

    Fixed input stream where program stops after first line break character

--- HW2-ScannerLab/HW2.c
+++ HW2-ScannerLab/HW2.c
@@ -25,67 +25,72 @@ void scanner(char* filename, const char* pattern) {
     FILE *in;
     char input[BUFFER_SIZE]; // input buffer
     in = fopen(filename, "r");
-    fgets(input, BUFFER_SIZE, in);
-    fclose(in);
+    // fgets(input, BUFFER_SIZE, in);
     
     // output
     char* outputFilename = strreplace(filename, pattern, "output");
     FILE *out;
-    out = fopen(outputFilename, "a");
+    out = fopen(outputFilename, "w");
 
     // scanning
-    int i = 0;
-    while (input[i] != '\0') {
-        char ch = input[i];
-        
-        // skip spaces
-        if (isspace(ch)) {
-            i++;
-            continue;
-        }
-        // if digit
-        else if (isdigit(ch)) {
-            char numBuffer[BUFFER_SIZE];
-            int index = 0;
+    while (fgets(input, BUFFER_SIZE, in)) {
+        int i = 0;
+        while (i != strlen(input)) {
+            char ch = input[i];
             
-            // look for succeeding digits
-            while (isdigit(ch)) {
-                numBuffer[index++] = ch;
-                // printf("added %c\n", ch);
-                ch = input[++i];
+            // skip spaces
+            if (isspace(ch)) {
+                i++;
+                continue;
             }
-            numBuffer[index] = '\0'; // null terminate string
+            // if digit
+            else if (isdigit(ch)) {
+                char numBuffer[BUFFER_SIZE];
+                int index = 0;
+                
+                // look for succeeding digits
+                while (isdigit(ch)) {
+                    numBuffer[index++] = ch;
+                    // printf("added %c\n", ch);
+                    ch = input[++i];
+                }
+                numBuffer[index] = '\0'; // null terminate string
 
-            // output number
-            fprintf(out, "NUM\t%s\n", numBuffer);
+                // output number
+                fprintf(out, "NUM\t%s\n", numBuffer);
 
-            // push back
-            i--;
+                // push back
+                i--;
 
-            // empty num buffer
-            index = 0;
-            numBuffer[0] = '\0';
-        }
-        else if (ch == '=') {
-            if (input[++i] == '=')
-                fprintf(out, "ASSIGN\t==\n");
+                // empty num buffer
+                index = 0;
+                numBuffer[0] = '\0';
+            }
+            else if (ch == '=') {
+                char next = input[++i];
+                if (next == '=')
+                    fprintf(out, "ASSIGN\t==\n");
+                else {
+                    fprintf(out,"Lexical Error reading character \"%c\"", next);
+                    break;
+                }
+            }
+            else if (ch == '+') {
+                fprintf(out, "PLUS\t+\n");
+            }
+            else if (ch == '-') {
+                fprintf(out, "MINUS\t-\n");
+            }
             else {
-                fprintf(out,"Lexical Error reading character \"=\"", ch);
+                fprintf(out,"Lexical Error reading character \"%c\"", ch);
                 break;
             }
+            i++;
         }
-        else if (ch == '+') {
-            fprintf(out, "PLUS\t+\n");
-        }
-        else if (ch == '-') {
-            fprintf(out, "MINUS\t-\n");
-        }
-        else {
-            fprintf(out,"Lexical Error reading character \"%c\"", ch);
-            break;
-        }
-        i++;
     }
+        
+    fclose(in);
+    fclose(out);
 }
 
 void application(const char* pattern) {
